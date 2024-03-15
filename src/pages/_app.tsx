a490945cb23c131b0acad7db24076ebd633bcee1
Vinicius Reis <vinireis010305@gmail.com>
2024-03-15 20:09:45 -0300

    Estrutura visual - Cabeçalho da aplicação

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,21 @@
 import { AppProps } from "next/app"
 import { globalStyles } from "../../styles/global"
+import { Container, Header } from "../../styles/pages/app"
+import logoImg from '../assets/Logo.svg'
+import Image from "next/future/image"
 
 globalStyles()
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="logo da empresa"/>
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
 
 export default App
