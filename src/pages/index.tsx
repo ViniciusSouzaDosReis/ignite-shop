52f999d900bfb5b857965c8d34634f362bd74a07
Vinicius Reis <vinireis010305@gmail.com>
2024-03-15 20:09:51 -0300

    Estrutura visual - Estrutura da Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,30 @@
 import { styled } from "../../styles"
+import { HomeContainer, Product } from "../../styles/pages/home"
+import Image from "next/future/image"
+import camiseta1 from '../assets/camisetas/+PLUS-T-shirttransparente 1.png'
+import camiseta2 from '../assets/camisetas/2_explorer-t-shirt 1.png'
 
-const Button = styled('button', {
-  backgroundColor: '$green300',
-})
 
 export default function Home() {
   return (
-    <Button>OLA</Button>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={camiseta2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
